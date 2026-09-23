32a4b222634b213bf787d81c7fa838d67da614ca
Varun Patil <varunbpatil1121@gmail.com>
2026-09-23 18:54:07 +0530

    Update cinema expense amount for accuracy

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
     ),
     Expense(
       title: 'Cinema',
-      amount:3.15, 
+      amount:7.18,
       date: DateTime.now(),
       category: Category.leisure,
     ),
